2d52b38b3605327e55c7bf840fc94545fc6eeb7c
Matheus <130152350+MatheuszJesus@users.noreply.github.com>
2023-12-07 20:58:52 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types = 1);
+
+session_start();
+
+use Contrafile\Core\Router;
+
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__. "/app/config.php";
+require __DIR__. "/app/rotas.php";
+require __DIR__. "/app/Core/helper.php";
+
+$url = $_GET['url'] ?? "";
+unset($_GET['url']);
+$metodoHTTP = $_SERVER["REQUEST_METHOD"];
+Router::exec($url, $metodoHTTP);
